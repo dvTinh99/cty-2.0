7dd34a496693826a3eab51f32d5df8fafa8ff95d
t <tinh.doan@codecomplete.jp>
2023-10-27 08:40:04 +0700

    update

--- layout/index_content.php
+++ layout/index_content.php
@@ -146,11 +146,11 @@ Tập hợp những kỹ thuật - công nghệ hàng đầu ứng dụng vào q
 
     <!-- Features Start -->
     
-    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s">
+    <div class="container-fluid py-5 wow fadeInUp" data-wow-delay="0.1s" style="text-transform: uppercase;">
         <div class="container py-5">
             <div class="section-title text-center position-relative pb-3 mb-5 mx-auto" style="max-width: 600px;">
-                <h5 class="fw-bold text-primary text-uppercase">Tại sao chọn chúng tôi</h5>
-                <h1 class="mb-0">Uy tín tạo ra sức mạnh</h1>
+                <h5 class="fw-bold text-primary text-uppercase">TẠI SAO CHỌN CHÚNG TÔI</h5>
+                <h1 class="mb-0">UY TÍN TẠO RA SỨC MẠNH</h1>
             </div>
             <div class="row g-5">
                 <div class="col-lg-4">
